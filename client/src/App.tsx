01420b6abaf7827292fc4904d8c65b54e15a6b70
Lue Xiong <luexiong@lues-mbp.lan>
2022-12-13 13:56:44 -0600

    Add sagewhale api client and env var reader

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,9 @@
 import { ChakraProvider, VStack, Grid, theme } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import FileUploadPage from "./components/FileUploadPage";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 export const App = () => (
   <ChakraProvider theme={theme}>
---
+++ client/src/apis/sageWhaleApi.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const SUBSCRIBER_INFO_ROUTE = "/subscriber_info";
+const UPLOAD_JSON_ROUTE = `${SUBSCRIBER_INFO_ROUTE}/upload/json`;
+
+const sageWhaleApi = axios.create({
+  baseURL: process.env.SAGEWHALE_API_URL,
+});
+
+const getAllSubscriberInfo = function () {
+  return sageWhaleApi.get(SUBSCRIBER_INFO_ROUTE);
+};
+
+const postJsonFile = function (file: FormData) {
+  return sageWhaleApi.post(UPLOAD_JSON_ROUTE, file, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+};
+
+export default { getAllSubscriberInfo, postJsonFile };
