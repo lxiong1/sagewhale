cf96a1192e8590766d2852a974f7828b64d7159d
Lue Xiong <luexiong@lues-mbp.lan>
2022-12-13 13:28:35 -0600

    Change app test case to check for color switch

--- client/src/__tests__/App.test.tsx
+++ client/src/__tests__/App.test.tsx
@@ -1,10 +1,18 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { App } from "../App";
 
-test("Should render color mode button", () => {
+test("Should switch color mode on click", async () => {
   render(<App />);
+  const user = userEvent.setup();
 
   expect(
     screen.getByRole("button", { name: "Switch to dark mode" })
+  ).toBeInTheDocument();
+
+  await user.click(screen.getByRole("button", { name: "Switch to dark mode" }));
+
+  expect(
+    screen.getByRole("button", { name: "Switch to light mode" })
   ).toBeInTheDocument();
 });
